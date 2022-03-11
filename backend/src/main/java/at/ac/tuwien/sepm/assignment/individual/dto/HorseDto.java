4457606f3150b6fec3ae976c00577e0c1c3ca08b
Jakob R <Sicari-s@users.noreply.github.com>
2022-03-11 10:43:02 +0100

    [US 1] udpated HorseDto to respect new properties

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/dto/HorseDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/dto/HorseDto.java
@@ -1,12 +1,13 @@
 package at.ac.tuwien.sepm.assignment.individual.dto;
 
+import at.ac.tuwien.sepm.assignment.individual.enums.HorseBiologicalGender;
+
+import java.time.LocalDate;
+
 /**
  * Class for Horse DTOs
  * Contains all common properties
  */
-public record HorseDto(Long id, String name, description
-        birthdate
-sex
-                               owner) {
+public record HorseDto(Long id, String name, String description, LocalDate birthdate, HorseBiologicalGender sex, String owner) {
 
 }
