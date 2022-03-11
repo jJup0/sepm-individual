f3d05244ba45396af0baffc3728b9322baea6ddd
Jakob R <Sicari-s@users.noreply.github.com>
2022-03-11 10:27:03 +0100

    [US 1] added requested fields to table horse

---
+++ backend/src/main/resources/sql/createSchema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS horse
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    description     VARCHAR(255) NULL,
+    birthdate       DATE NOT NULL,
+    sex             VARCHAR(6) NOT NULL CHECK (sex in ('male', 'female')),
+    owner           VARCHAR(255) NULL
+);
