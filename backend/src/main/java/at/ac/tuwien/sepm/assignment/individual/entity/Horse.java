17548d04c8c443ef9988e3b0e26c97714011d1ef
Jakob R <Sicari-s@users.noreply.github.com>
2022-03-11 10:40:21 +0100

    [US 1] changed Horse entity to account for newly introduced properties

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/entity/Horse.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/entity/Horse.java
@@ -3,7 +3,10 @@ package at.ac.tuwien.sepm.assignment.individual.entity;
 public class Horse {
     private Long id;
     private String name;
-    private
+    private String description;
+    private String birthdate;
+    private String sex;
+    private String owner;
 
     public Long getId() {
         return id;
@@ -21,4 +24,35 @@ public class Horse {
         this.name = name;
     }
 
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
