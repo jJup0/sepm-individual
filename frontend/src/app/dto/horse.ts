432b99867dbfecb07fb2a1c4a1a2ee63dd421e1c
Jakob R <Sicari-s@users.noreply.github.com>
2022-03-11 11:00:31 +0100

    [US 1] added horse sex union type in front end and updated horse dto

--- frontend/src/app/dto/horse.ts
+++ frontend/src/app/dto/horse.ts
@@ -1,4 +1,10 @@
+import { HorseSex } from "./horseSex";
+
 export interface Horse {
   id?: number;
   name: string;
+  description: string;
+  birthdate: Date;
+  sex: HorseSex;
+  owner: string;
 }
---
+++ frontend/src/app/dto/horseSex.ts
@@ -0,0 +1,1 @@
+export type HorseSex = "male" | "female";
