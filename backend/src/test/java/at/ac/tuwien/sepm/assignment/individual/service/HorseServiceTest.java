477731faf5b7e4b0979f51beb769d5b03fa552c5
Jakob R <Sicari-s@users.noreply.github.com>
2022-03-12 09:47:20 +0100

    [US 1] added tests in service layer for adding horses

--- backend/src/test/java/at/ac/tuwien/sepm/assignment/individual/service/HorseServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/assignment/individual/service/HorseServiceTest.java
@@ -1,26 +1,67 @@
 package at.ac.tuwien.sepm.assignment.individual.service;
 
+import at.ac.tuwien.sepm.assignment.individual.dto.HorseDto;
 import at.ac.tuwien.sepm.assignment.individual.entity.Horse;
+
+import java.time.LocalDate;
 import java.util.List;
+
+import at.ac.tuwien.sepm.assignment.individual.enums.HorseBiologicalGender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ActiveProfiles({"test", "datagen"}) // enable "test" spring profile during test execution in order to pick up configuration from application-test.yml
+@ActiveProfiles({"test", "datagen"})
+// enable "test" spring profile during test execution in order to pick up configuration from application-test.yml
 @SpringBootTest
 public class HorseServiceTest {
 
-  @Autowired
-  HorseService horseService;
+    @Autowired
+    HorseService horseService;
 
-  @Test
-  public void getAllReturnsAllStoredHorses() {
-    List<Horse> horses = horseService.allHorses();
-    assertThat(horses.size()).isEqualTo(1);
-    assertThat(horses.get(0).getId()).isEqualTo(-1);
-    assertThat(horses.get(0).getName()).isEqualTo("Wendy");
-  }
+    @Test
+    public void getAllReturnsAllStoredHorses() {
+        List<Horse> horses = horseService.allHorses();
+        assertThat(horses.size()).isEqualTo(1);
+        assertThat(horses.get(0).getId()).isEqualTo(-1);
+        assertThat(horses.get(0).getName()).isEqualTo("Wendy");
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    public void addOneHorseValid() {
+
+        HorseDto newHorseDto = new HorseDto(null, "test horse 1", "test description 1", LocalDate.now(), HorseBiologicalGender.male, "");
+        Horse addedHorse = horseService.addHorse(newHorseDto);
+        assertThat(addedHorse.getId()).isNotNull();
+
+
+    }
+
+    @Test
+    // TODO find better way
+    public void addOneHorseInvalid() {
+
+        HorseDto badHorses[] = {
+                new HorseDto(null, null, "test description 1", LocalDate.now(), HorseBiologicalGender.male, ""),
+                new HorseDto(null, null, "test description 1", null, HorseBiologicalGender.male, ""),
+                new HorseDto(null, null, "test description 1", LocalDate.now(), null, "")};
+
+        boolean failed;
+        for (int i = 0; i < badHorses.length; i++) {
+            failed = false;
+            try {
+                horseService.addHorse(badHorses[i]);
+            } catch (Exception e) {// TODO make specific exception
+                failed = true;
+            }
+            assertThat(failed).isTrue();
+        }
+
+    }
+
 }
